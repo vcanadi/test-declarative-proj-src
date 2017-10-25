176d490d32340b9b5ec124a4a1952e59cebd1ac4
vito canadi <vito.canadi@gmail.com>
2017-10-25 12:25:56 +0200

    Maintainer added

--- jobs/release.nix
+++ jobs/release.nix
@@ -5,25 +5,28 @@ let
   in
   with pkgs;
   pkgs.releaseTools.nixBuild {
-  name = "simple";
-  builder = "${pkgs.bash}/bin/bash";
-  args = [( builtins.toFile "tmp_builder.sh" '' 
-    echo -------------------
-    echo ----IN_BUILDER-----
-    echo -------------------
-    export PATH="$coreutils/bin:$gcc/bin"
-    mkdir $out
-    gcc -o $out/simple $src
-  '')];
+    name = "simple";
+    builder = "${pkgs.bash}/bin/bash";
+    args = [( builtins.toFile "tmp_builder.sh" '' 
+      echo -------------------
+      echo ----IN_BUILDER-----
+      echo -------------------
+      export PATH="$coreutils/bin:$gcc/bin"
+      mkdir $out
+      gcc -o $out/simple $src
+    '')];
 
-  inherit gcc coreutils;
+    inherit gcc coreutils;
 
-  src = builtins.toFile "tmp_simple.c" ''
-    void main () { 
-      puts ("Simple!");
-    }
-  '';
-  system = builtins.currentSystem;
+    src = builtins.toFile "tmp_simple.c" ''
+      void main () { 
+        puts ("Simple!");
+      }
+    '';
+    system = builtins.currentSystem;
+    meta = {
+      maintainer = "vito.canadi@gmail.com";
+    };
   };
 in {
   hs-job0-0 = hs-job0; 
