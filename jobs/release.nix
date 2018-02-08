39fee61aafcd3c7064e66328abb00f795d394803
Vito Canadi <vito.canadi@gmail.com>
2018-02-08 03:59:32 +0100

    Minimal release.nix

--- jobs/release.nix
+++ jobs/release.nix
@@ -1,15 +1,8 @@
-let
+{
   hs-job0 = { nixpkgs ? <nixpkgs> } :
-  let
-    pkgs = import nixpkgs {};
-  in
-  pkgs.releaseTools.nixBuild {
-    name = "simple-program";
-    buildCommand = ''touch $out '';
-    passAsFile = [ "buildCommand" ];
-    src = "";
-  };
-in {
-  hs-job0-0 = hs-job0;
+    let
+      pkgs = import nixpkgs {};
+    in
+    pkgs.runCommand "hs-job-name" {} "";
 }
 
